76c4f1ca2d66ae4213894b88d1430eeacee75077
SultanBashier <sultan.alhejeery@gmail.com>
2025-08-09 03:25:11 +0300

    E-Book

---
+++ bulky.Utility/SD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bulkyBook.Utility
+{
+    public static class SD
+    {
+        //This The Roles Of The Users
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+
+        //This The Status Of The Payment
+        public const string StatusPending= "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Proccessing";
+        public const string StatusShipped= "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string Statusrefunded = "Refunded";
+        
+        
+        
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
+        
+        
+        public const string SessionCart = "SessionShoppingCart";
+
+
+
+
+    
+    }
+}
